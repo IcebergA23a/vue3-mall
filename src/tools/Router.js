9ab085bd74c43b5e63ad2cd36b96ba69cfc106e6
IcebergA23a <thuliumkyg@gmail.com>
2025-12-27 17:30:57 +0800

    feat(订单管理): 添加订单管理路由及组件

--- src/tools/Router.js
+++ src/tools/Router.js
@@ -5,11 +5,24 @@ import Home from '../components/Home.vue';
 import Login from '../components/Login.vue';
 import Store from './Storage';
 
+// 引入Order组件
+import Order1 from '../components/order/Order1.vue'
+
 const Router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/login', component: Login , name: 'login'},
-        { path: '/home', component: Home, name: 'home' }
+        { path: '/home', component: Home, name: 'home',
+            // 新增订单管理模块的路由
+            children: [
+                { 
+                    path: 'order/:type', // 0 是普通订单，1 是秒杀订单
+                    component: Order1,
+                    name: "Order"
+                }
+            ],
+            redirect: '/home/order/0'
+         }
     ]
 });
 
