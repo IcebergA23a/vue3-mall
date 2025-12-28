95355b7daa39d8cb1067eb9d71c9825b6af44bf4
IcebergA23a <thuliumkyg@gmail.com>
2025-12-28 16:58:17 +0800

    feat(订单管理): 1、 增加订单列表表格 2、多选处理 3、联系客户 4、删除订单

--- src/components/order/Order1.vue
+++ src/components/order/Order1.vue
@@ -74,6 +74,47 @@
                 <el-tab-pane label="已发货"></el-tab-pane>
                 <el-tab-pane label="支付超时"></el-tab-pane>
             </el-tabs>
+
+            <!-- table -->
+            <el-table
+            ref="multipleTable"
+            :data="orderList"
+            tooltip-effect="dark"
+            style="width: 100%"
+            @selection-change="handleSelectionChange"> 
+                <el-table-column type="selection" width="55"></el-table-column>
+                <el-table-column label="商品" width="100" prop="name"></el-table-column>
+                <el-table-column label="总价/数量" width="100" prop="price"></el-table-column>
+                <el-table-column label="买家信息" width="100" prop="buyer"></el-table-column>
+                <el-table-column label="交易时间" width="200" prop="time"></el-table-column>
+                
+                <el-table-column label="分销信息" width="100">
+                    <template #default="scope">
+                        <el-tag size="default" :type="scope.row.role ? 'primary' : 'info' ">
+                            {{ scope.row.role ? '经理' : '分销员' }}
+                        </el-tag>
+                    </template>
+                </el-table-column>
+                <el-table-column label="状态" width="100">
+                    <template #default="scope">
+                        <el-tag size="default" :type="scope.row.state ? 'success' : 'danger'">
+                            {{ scope.row.state ? '已完成' : '未完成' }}
+                        </el-tag>          
+                    </template>               
+                </el-table-column>
+                <el-table-column label="操作" width="200">
+                    <template #default="scope">
+                        <el-button size="small" type="danger" @click="deleteItem(scope.$index)">删除</el-button>
+                        <el-button size="small" type="primary" @click="callUser(scope.row)">联系客户</el-button>            
+                    </template>     
+                </el-table-column>
+                <el-table-column label="支付方式" width="100">
+                    <template #default="scope">
+                        <el-tag size="default">{{ scope.row.payType ? '微信' : '支付宝' }}</el-tag>          
+                    </template>
+                </el-table-column>
+                <el-table-column label="来源" width="200" prop="source"></el-table-column>
+            </el-table>
         </div>
 
      </div>
@@ -95,7 +136,9 @@
                     name: '',
                     payTime: '',
                     sendTime: ''
-                }
+                },
+                // 当前选中的订单对象
+                multipleSelection:[]
             }
         },
         methods: {
@@ -114,6 +157,22 @@
                     message:'切换tab刷新数据：' + tab.props.label
                 })
                 this.orderList = Mock.getOrder(this.$route.params.type);
+            },
+            // 处理多选
+            handleSelectionChange(val) {
+                this.multipleSelection = val
+            },
+            // 删除订单
+            deleteItem(item) {
+                this.orderList.splice(item, 1)
+            },
+            // 联系用户
+            callUser(item) {
+                console.log(item)
+                this.$message({
+                    type: 'success',
+                    message:'联系客户：' + item.phone
+                })
             }
 
 
