e6b31946d4d805f2d16b3c518039eddaf28e5fa8
IcebergA23a <thuliumkyg@gmail.com>
2026-01-11 16:29:24 +0800

    feat(user-management):  注销

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,6 +6,7 @@
             <el-menu-item index="3" v-if="!isLoggedIn"><router-link to="/register">Register</router-link></el-menu-item>
             <el-menu-item index="4" v-if="isLoggedIn"><router-link to="/profile">Profile</router-link></el-menu-item>
             <el-menu-item index="5" v-if="isLoggedIn && isAdmin"><router-link to="/admin">Admin</router-link></el-menu-item>
+            <el-menu-item index="6" v-if="isLoggedIn" @click="logout">Logout</el-menu-item>
         </el-menu>
     </el-header>
 </template>
@@ -18,9 +19,15 @@
 <script setup>
     import { computed } from 'vue';
     import { useAuthStore } from '@/stores/authStore';
+    import { useRouter } from 'vue-router';
 
     const authStore = useAuthStore();
     const isLoggedIn = computed(() => authStore.isLoggedIn);
     const isAdmin = computed(() => authStore.user?.role === 1);
 
+    const router = useRouter();
+    const logout = () => {
+        authStore.logout();
+        router.push('/');
+    };
 </script>
